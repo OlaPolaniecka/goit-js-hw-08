3e1f282207872bba4d315f1aab967e42aa9ceb51
Ola Połaniecka <97olka@gmail.com>
2023-11-17 08:41:34 +0100

    java8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+for (let item of galleryItems) {
+  let gallery__element = document.createElement('li');
+  let gallery__link = document.createElement('a');
+  let gallery__image = document.createElement('img');
+  gallery__element.classList.add('gallery__element');
+  gallery__link.classList.add('gallery__item');
+  gallery__image.classList.add('gallery__image');
+  gallery__link.setAttribute('href', item.original);
+  gallery__image.setAttribute('src', item.preview);
+  gallery__image.setAttribute('data-source', item.original);
+  gallery__image.setAttribute('alt', item.description);
+  gallery__link.append(gallery__image);
+  gallery__element.append(gallery__link);
+  gallery.append(gallery__element);
+}
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('form');
+const emailInput = form.querySelector('label > input');
+let textInput = form.querySelector('label > textarea');
 
+form.addEventListener(
+  'input',
+  _.throttle(e => {
+    let data = {
+      email: emailInput.value,
+      message: textInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }, 500)
+);
+
+let getData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (localStorage.getItem('feedback-form-state') === null) {
+  emailInput.value = '';
+  textInput.value = '';
+} else {
+  emailInput.value = getData.email;
+  textInput.value = getData.message;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  emailInput.value = '';
+  textInput.value = '';
+  localStorage.removeItem('feedback-form-state');
+});
